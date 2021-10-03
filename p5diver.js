f72780edd03a6b764c25a827d3374e8a3c3cb196
Nuvie <joel.frehner@gmail.com>
2021-10-03 19:10:18 +0200

    changes

--- p5diver.js
+++ p5diver.js
@@ -1,12 +1,102 @@
+let n = [
+  {
+    1: {
+      first: 5,
+      second: 4,
+      third: 7,
+      forth: 3,
+      fifth: 9,
+      sixth: 8,
+      seventh: 6,
+      eight: 3,
+      maxSeqLen: 1,
+      setSize: 7,
+      highestFrequency0: 2,
+      highestFrequency1: 1,
+      highestFrequency2: 1,
+      sum: 45,
+    },
+  },
+  {
+    2: {
+      first: 8,
+      second: 4,
+      third: 8,
+      forth: 9,
+      fifth: 7,
+      sixth: 5,
+      seventh: 9,
+      eight: 9,
+      maxSeqLen: 2,
+      setSize: 5,
+      highestFrequency0: 3,
+      highestFrequency1: 2,
+      highestFrequency2: 1,
+      sum: 59,
+    },
+  },
+  {
+    3: {
+      first: 9,
+      second: 11,
+      third: 6,
+      forth: 8,
+      fifth: 5,
+      sixth: 1,
+      seventh: 5,
+      eight: 6,
+      maxSeqLen: 1,
+      setSize: 6,
+      highestFrequency0: 2,
+      highestFrequency1: 2,
+      highestFrequency2: 1,
+      sum: 51,
+    },
+  },
+];
+
 let weapons = [
   "./imges/accesoirs/1.png",
   "./imges/accesoirs/2.png",
   "./imges/accesoirs/3.png",
   "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
+  "./imges/accesoirs/4.png",
 ];
+
+let suits = [
+  "./imges/suits/1.png",
+  "./imges/suits/2.png",
+  "./imges/suits/3.png",
+  "./imges/suits/2.png",
+  "./imges/suits/3.png",
+  "./imges/suits/1.png",
+  "./imges/suits/2.png",
+  "./imges/suits/1.png",
+  "./imges/suits/3.png",
+  "./imges/suits/2.png",
+  "./imges/suits/3.png",
+  "./imges/suits/1.png",
+  "./imges/suits/3.png",
+  "./imges/suits/2.png",
+  "./imges/suits/3.png",
+];
+
+let currN = 0;
 
 let weapon = weapons[0];
 let currWeapon = 0;
+
+let currSuit = 0;
 
 let maskIndex = 0,
   tankIndex = 0,
@@ -23,9 +113,11 @@ function setup() {
   var ddCanvas = createCanvas(1116, 1116);
   ddCanvas.parent("ddCanvas");
 
+  frameRate(1);
+
   stroke(255);
 
-  //back = loadImage("./imges/body2.png");
+  back = loadImage("./imges/design/start.gif");
   body = loadImage("./imges/body.png");
   fins = loadImage("./imges/fins.png");
   masks = loadImage("./imges/masks.png");
@@ -36,19 +128,67 @@ function setup() {
   weapon2 = loadImage(weapons[1]);
   weapon3 = loadImage(weapons[2]);
   weapon4 = loadImage(weapons[3]);
+
+  suit1 = loadImage(suits[0]);
+  suit2 = loadImage(suits[1]);
+  suit3 = loadImage(suits[2]);
+  suit4 = loadImage(suits[3]);
+  suit5 = loadImage(suits[4]);
+  suit6 = loadImage(suits[5]);
+  suit7 = loadImage(suits[6]);
+  suit8 = loadImage(suits[7]);
+  suit9 = loadImage(suits[8]);
+  suit10 = loadImage(suits[9]);
+  suit11 = loadImage(suits[10]);
+  suit12 = loadImage(suits[11]);
+  suit13 = loadImage(suits[12]);
+  suit14 = loadImage(suits[13]);
+  suit15 = loadImage(suits[14]);
 }
 
 function draw() {
   // image(back, 0, 0)
   clear();
+
+  let currWep = n[currN][currN + 1]["first"];
+
   var weps = [weapon1, weapon2, weapon3, weapon4];
   var wep = weps[currWeapon];
 
+  var suits = [
+    suit1,
+    suit2,
+    suit3,
+    suit4,
+    suit5,
+    suit6,
+    suit7,
+    suit8,
+    suit9,
+    suit10,
+    suit11,
+    suit12,
+    suit13,
+    suit14,
+    suit15,
+  ];
+  var suit = suits[currSuit];
+
+  image(back, 0, 0);
   image(body, 0, 0);
+  image(suit, 0, 0);
   image(fins, finXPos, 0);
   image(masks, maskXPos, 0);
   image(tanks, tankXPos, 0);
   image(wep, accessoirXPos, 0);
+
+  /*
+  if (currSuit < 14) {
+    currSuit++;
+  } else {
+    currSuit = 0;
+  }
+  */
 }
 
 function getXPos(index, currXpos, right) {
@@ -119,6 +259,23 @@ accessoirR.addEventListener("click", function () {
   currWeapon++;
   if (currWeapon >= weapons.length) {
     currWeapon = 0;
+  }
+});
+
+// Suits
+var suitL = document.getElementById("suitl");
+var suitR = document.getElementById("suitr");
+suitL.addEventListener("click", function () {
+  if (currSuit == 0) {
+    currSuit = suits.length - 1;
+  } else {
+    currSuit--;
+  }
+});
+suitR.addEventListener("click", function () {
+  currSuit++;
+  if (currSuit >= suits.length) {
+    currSuit = 0;
   }
 });
 
